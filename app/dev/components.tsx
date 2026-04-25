7752ce7acf87c9f24ae6a3e3ec8e936b19f61bce
Dev Yadav <devy19372@gmail.com>
2026-04-25 08:11:10 +0530

    Update components.tsx

--- app/dev/components.tsx
+++ app/dev/components.tsx
@@ -1,5 +1,3 @@
-// A development-only route allowing Dazl to render component previews.
-
 import "@dazl/component-gallery/styles.css";
 export { default } from "@dazl/component-gallery/route";
 export function clientLoader() {}
